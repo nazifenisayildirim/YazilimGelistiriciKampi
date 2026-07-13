f18a565ce6d232dde0ae0c217bd8a739886c6e44
Nazife Nisa Yıldırım <nazifenisayildirim@gmail.com>
2026-07-13 11:24:11 +0300

    Add Python basics examples

---
+++ degiskenler.py
@@ -0,0 +1,32 @@
+
+baslik = 'İlginizi Çekebilir!'
+
+print(baslik)
+
+print(baslik)
+
+print(baslik)
+
+print(baslik)
+
+print(baslik)
+
+"""
+print('İlginizi Çekebilir!') #string
+print('İlginizi Çekebilir')
+print('İlginizi Çekebilir!')
+print('İlginizi Çekebilir')
+print('İlginizi Çekebilir')
+"""
+
+vade = 48
+print(vade)
+faizOrani = 1.69
+print(faizOrani)
+
+print(type(faizOrani))
+print(type(vade))
+print(type(baslik))
+print(vade + vade)
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+kredi1 = "HızlıKredi"
+kredi2 = "Maaşını Halkbanktan..."
+kredi3 = "Mutlu emekli"
+
+print(kredi1)
+print(kredi2)
+print(kredi3)
+
+krediler = ["HızlıKredi","Maaşını Halkbanktan...","Mutlu emekli"]
+
+for kredi in krediler:
+    print(kredi)
